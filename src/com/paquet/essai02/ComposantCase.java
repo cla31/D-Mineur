5a5117215c95a55485ff4add4938fca496d71d36
claire.mercat <claire.mercat@FR-L4228411.groupinfra.com>
2019-06-22 14:49:35 +0200

    Test du JToggle bouton, ses evenements souris, sa couleur de fond selon les actions de la souris.

---
+++ src/com/paquet/essai02/ComposantCase.java
@@ -0,0 +1,142 @@
+package com.paquet.essai02;
+
+/**
+ * Paquet qui permet de tester le JToggle bouton, ses evènements souris et sa couleur de fond.
+ * @author claire
+ */
+
+import java.awt.Color;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.io.PrintStream;
+
+import javax.swing.BorderFactory;
+import javax.swing.JToggleButton;
+import javax.swing.SwingUtilities;
+import javax.swing.border.Border;
+
+/**
+ * <p>ComposantCase correspond aux objets bouton.
+ * On utilise un JToggleButton.
+ * Un JButton est un bouton classique.
+ * Un JToggleButton réagit comme une boite coché ou un interrupteur.
+ * Si tu appuie dessus il reste enfoncé, situ ré-appuie dessus,il reprendra son état initial.
+ * 
+ * On implémente l'interface MouseListener ainsi que toutes les méthodes liées à l'interface.<p>
+ * 
+ */
+
+public class ComposantCase extends JToggleButton implements MouseListener {
+	private String name;
+	/**
+	 * Insruction qui définit les bordures du bouton,
+	 * ainsi que la couleur de la bordure.
+	 */
+	Border lineBorder = BorderFactory.createLineBorder(Color.GRAY, 1);
+
+	public ComposantCase(String str) {
+
+		super(str);
+		/**
+		 * Insruction qui définit les bordures du bouton
+		 */
+		this.setBorder(lineBorder);
+		this.setOpaque(true);
+		
+		this.name=str;
+		/**
+		 * Grâce à cette instruction, notre objet va s'écouter
+		 * pour repérer les actions de la souris(appui sur le bouton, entrée/sortie du  curseur)
+		 * @param this (c'est à dire l'objet lui-même)
+		 */
+		this.addMouseListener(this);
+	}
+     /**
+      * cliquer
+      */
+	@Override
+	public void mouseClicked(MouseEvent e) {
+//		 setBackground(Color.green);
+//		 System.out.println("Mouse is Clicked");
+
+	}
+
+	/**
+	 * Invoqué lorsque la souris entre dans un composant.
+	 */
+	@Override
+	public void mouseEntered(MouseEvent e) {
+//		setBackground(Color.cyan);
+//	    System.out.println("Mouse is Entered");
+	  
+
+	}
+	/**
+	 * Invoqué lorsque la souris quitte un composant.
+	 */
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		
+//		 setBackground(Color.magenta);
+//		 System.out.println("Mouse is Exited");
+	}
+	
+	/**
+	 * Appuyer sur le bouton
+	 */
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		if (this.isEnabled()) {
+            /**
+             * SwingUtilities?
+             * Quand on crée un composant, un thread specifique est chargé de la gestion des évenements et
+             * à partir du moment où notre composant est visible
+             * toutes les modifications affectant ce composant doit etre faite par ce thread,
+             * c'est là qu'intervient notre classe SwingUtilities. 
+             */
+			if (SwingUtilities.isRightMouseButton(e)) {
+				System.out.println("Bouton droit pressé");
+				this.setBackground(Color.YELLOW);}
+		}
+
+		else{
+			this.setText("P");
+			this.setBackground(Color.BLACK);
+
+		}
+
+
+	}
+	/**
+	 * Relâcher le bouton
+	 */
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+
+		if(this.isEnabled()) {
+			if (SwingUtilities.isRightMouseButton(e)) {
+				System.out.println("Bouton droit relâché");
+				this.setBackground(Color.RED);
+			}
+			else {
+				this.setText("R");
+				this.setEnabled(false);
+				this.setBackground(Color.RED);
+
+			}
+		}
+
+
+
+	}
+	
+	 
+
+
+
+
+}
+
